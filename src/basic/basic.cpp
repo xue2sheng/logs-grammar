9baf8de2ca6d7a6b4e1af22bb827b56845eb9541
Andres Sevillano <andres.sevillano.vallejo@gmail.com>
2015-11-05 02:31:43 +0000

    Basic example

--- src/basic/basic.cpp
+++ src/basic/basic.cpp
@@ -1,5 +1,9 @@
 #include "basic.hpp"
 
-extern "C" basic* create() { return new basic; }
+std::string basic::id() const
+{
+    return "basic_library";
+}
 
+extern "C" basic* create() { return new basic; }
 extern "C" void destroy(basic* b) { delete b; }
