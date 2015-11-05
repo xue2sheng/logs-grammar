96225c0532c57768912e3808e0cf8bb0bfe9322e
Andres Sevillano <andres.sevillano.vallejo@gmail.com>
2015-11-05 02:32:12 +0000

    Basic example

--- src/basic/basic.hpp
+++ src/basic/basic.hpp
@@ -1,21 +1,27 @@
 #ifndef BASIC_HPP
 #define BASIC_HPP
 
+#include <string>
+
 class basic {
 public:
 	basic() {}
 	virtual ~basic() {}
+    std::string id() const;
 };
 
 typedef basic* create_t();
 typedef void destroy_t(basic*);
 
 #if defined(__linux__) || defined(__sun__)
-static const char* LIB_EXT =  ".so";
+static const std::string LIB_PRE = "lib";
+static const std::string LIB_EXT = ".so";
 #elif defined(__APPLE__)
-static const char* LIB_EXT = ".dylib";
+static const std::string LIB_PRE = "lib";
+static const std::string LIB_EXT = ".dylib";
 #else
-static const char* LIB_EXT = "";
+static const std::string LIB_PRE = "";
+static const std::string LIB_EXT = "";
 #endif
 
 #endif
